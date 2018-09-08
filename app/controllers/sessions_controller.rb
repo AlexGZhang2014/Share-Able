751545a0b8b83ec1da2349adf42900eae7311ad7
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 18:47:14 -0400

    Define new, create, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Incorrect username and/or password."
+      redirect_to controller: "sessions", action: "new"
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
 end
