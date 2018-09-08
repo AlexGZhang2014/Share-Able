c90be06e8d5e433ca150a56f09bfdf22c6b929ec
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 19:06:26 -0400

    Change redirect after login to posts index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to posts_path
     else
       flash[:notice] = "Incorrect username and/or password."
       redirect_to controller: "sessions", action: "new"
