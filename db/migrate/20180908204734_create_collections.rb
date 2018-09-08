96c351a0b5244bc18178a453adac80a049d4110d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 16:49:07 -0400

    Add user_id to collection

--- db/migrate/20180908204734_create_collections.rb
+++ db/migrate/20180908204734_create_collections.rb
@@ -3,6 +3,7 @@ class CreateCollections < ActiveRecord::Migration[5.0]
     create_table :collections do |t|
       t.string :name
       t.string :description
+      t.integer :user_id
 
       t.timestamps
     end
