130970b160d11e12f9641e317a09210c91a757e8
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 18:47:54 -0400

    Define new, create, and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
