365c82f68c5ad07b792f6fcd6c1fb14c09b50b0a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 21:23:17 -0400

    Add validations for author, content, and rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :collection, optional: true
   belongs_to :user, optional: true
+  validates :author, presence: true
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
