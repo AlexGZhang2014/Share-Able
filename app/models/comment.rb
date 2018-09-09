29e6bd67bb6b71ca7a5b6f288f401e777e4c510c
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 21:16:49 -0400

    Add validations for author and content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post, optional: true
   belongs_to :user, optional: true
+  validates :author, presence: true
+  validates :content, presence: true
 end
