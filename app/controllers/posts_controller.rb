b2fb0f3ccbdd5daac473f945a75c7c66660cbe35
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 21:43:57 -0400

    Define all standard CRUD actions for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,52 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to post_path(@post), notice: "Post was successfully created." }
+        format.json { render json: @post, status: 201 }
+      end
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "Post was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
