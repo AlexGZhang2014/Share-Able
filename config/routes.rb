b24f64236f3b9767d1fd56884d29eadb363a1e0d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-08 22:34:17 -0400

    Define nested Post resource for User

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :collections
   resources :comments
   resources :posts
-  resources :users
+  resources :users do
+    resources :posts, only: [:index, :new, :edit, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
