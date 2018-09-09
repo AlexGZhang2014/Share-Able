92260003102c158c7204fe43a24bc0e9256838f4
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-09 19:43:48 -0400

    Define new and create actions with appropriate comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to post_path(@comment.post), notice: "Comment was successfully created." }
+        format.json { render json: @comment, status: 201 }
+      end
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:author, :content, :user_id, :post_id)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 end
