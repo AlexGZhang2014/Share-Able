dd172d689f1dba2eab1e748918d3242a74da767a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-10 17:50:55 -0400

    Define helper methods and NEW action for Review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:edit, :update, :destroy]
+
+  def new
+    @review = Review.new(collection_id: params[:collection_id])
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:author, :content, :collection_id)
+    end
+
+    def set_review
+      @review = Review.find(params[:id])
+    end
 end
