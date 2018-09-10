6586221bb45398165ba2a3893490ac82a0e76c41
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-10 18:29:29 -0400

    Define EDIT and UPDATE actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,17 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to collection_path(@review.collection), notice: "Review was successfully updated."
+    else
+      render :edit
+    end
+  end
+
   private
 
     def review_params
