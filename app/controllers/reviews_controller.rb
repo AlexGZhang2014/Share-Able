335012192984d6abf8fda26ae94b3056c13897f7
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-10 18:16:14 -0400

    Define CREATE action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,10 +5,24 @@ class ReviewsController < ApplicationController
     @review = Review.new(collection_id: params[:collection_id])
   end
 
+  def create
+    @review = Review.new(review_params)
+    @review.user = @current_user
+    @review.author = @review.user.username
+    if @review.save
+      respond_to do |format|
+        format.html { redirect_to collection_path(@review.collection), notice: "Review was successfully created." }
+        format.json { render json: @review, status: 201 }
+      end
+    else
+      render :new
+    end
+  end
+
   private
 
     def review_params
-      params.require(:review).permit(:author, :content, :collection_id)
+      params.require(:review).permit(:author, :content, :rating, :collection_id)
     end
 
     def set_review
