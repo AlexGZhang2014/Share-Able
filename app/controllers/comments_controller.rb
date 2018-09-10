fdf065ed666090fe158b628b0e1e1dc1d7160fbb
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-09 20:35:50 -0400

    Modify new action so that post_id is being passed into the form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,13 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update, :destroy]
 
   def new
-    @comment = Comment.new
+    @comment = Comment.new(post_id: params[:post_id])
   end
 
   def create
     @comment = Comment.new(comment_params)
+    @comment.user = @current_user
+    @comment.author = @comment.user.username
     if @comment.save
       respond_to do |format|
         format.html { redirect_to post_path(@comment.post), notice: "Comment was successfully created." }
@@ -20,7 +22,7 @@ class CommentsController < ApplicationController
   private
 
     def comment_params
-      params.require(:comment).permit(:author, :content, :user_id, :post_id)
+      params.require(:comment).permit(:author, :content, :post_id)
     end
 
     def set_comment
