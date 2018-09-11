9a29bac884c7b9b7f4f4b77d953d6d69ca216108
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-10 21:48:59 -0400

    Add all necessary activerecord associations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
   has_many :posts
+  has_many :comments
+  has_many :collections
+  has_many :items, through: :collections
+  has_many :reviews
 end
