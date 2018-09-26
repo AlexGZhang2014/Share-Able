3e42350100ed2651987ed974513263f897e341a8
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-26 18:05:47 -0400

    Define more functions that successfully allow correct comment data and buttons to be displayed

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,6 +3,7 @@ class Comment {
     this.author = attributes.author;
     this.content = attributes.content;
     this.id = attributes.id;
+    this.post = attributes.post
   }
 
   static addCommentForm(e) {
@@ -14,9 +15,30 @@ class Comment {
       <label for="comment_content">Content</label><br>
       <textarea name="comment[content]" id="comment_content"></textarea>
       <br><br>
-      <input type="submit" name="commit" value="Create Comment" data-disable-with="Create Comment">
+      <input id="new_comment_submit" type="submit" name="commit" value="Create Comment" data-disable-with="Create Comment">
       </form>
       `);
+    $("#new_comment").on("submit", Comment.addNewComment)
+  }
+
+  static addNewComment(e) {
+    e.preventDefault();
+    let $formValues = $(this).serialize();
+    $.post("/comments", $formValues, Comment.successCreate, "json");
+  }
+
+  static successCreate(data) {
+    let comment = new Comment(data);
+    return comment.createComment();
+  }
+
+  createComment() {
+    $("#post_comments").append(`
+      <h4>${this.author} wrote:</h4>
+      <p>${this.content}</p>
+      <form class="button_to" method="get" action="/posts/${this.post.id}/comments/${this.id}/edit"><input type="submit" value="Edit this comment"></form>
+      <form class="button_to" method="post" action="/comments/${this.id}"><input type="hidden" name="_method" value="delete"><input data-confirm="Are you sure?" type="submit" value="Delete this comment"><input type="hidden" name="authenticity_token" value="oFtA5/vEpgl509urgzuQeAj2K+oDvvhtVU+PjS8VpzAVKNt9QIZURYR0k0Va8nrGmvBiTMf9qo11QQ4OVpLb1Q=="></form>
+    `);
   }
 
   static addJSListener() {
