8ef44f1984be26631772ab51894061abdc7f40e4
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-26 17:31:41 -0400

    Modify function so that it correctly appends the form that adds new comments to the page

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -7,7 +7,16 @@ class Comment {
 
   static addCommentForm(e) {
     e.preventDefault();
-    alert("got it");
+    $("#new_comment_form").append(`
+      <h1>Add a Comment</h1>
+      <form class="new_comment" id="new_comment" action="/comments" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="mrAkbYksI39GmD7MKEuvvJtNZT3gq9KfN1F36h48IAIvw7/3Mm7RM7s/diLxgkUCCUssmyTogH8XX/ZpZ7tc5w==">
+      <input type="hidden" value="2" name="comment[post_id]" id="comment_post_id">
+      <label for="comment_content">Content</label><br>
+      <textarea name="comment[content]" id="comment_content"></textarea>
+      <br><br>
+      <input type="submit" name="commit" value="Create Comment" data-disable-with="Create Comment">
+      </form>
+      `);
   }
 
   static addJSListener() {
